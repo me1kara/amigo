3006849936e8ddc5fc4cd8e4b70572e433d6182b
me1kara <aaddd2@naver.com>
2023-01-20 15:05:10 +0900

    실챗 방분기

--- amigo/src/main/java/com/lec/amigo/vo/ChatVO.java
+++ amigo/src/main/java/com/lec/amigo/vo/ChatVO.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class ChatVO {
 	
 	private int index;
-	private int user_no;
+	private String user_nick;
 	private String content;
 	private Date date;
 	private boolean read_is;
@@ -17,11 +17,12 @@ public class ChatVO {
 	public void setIndex(int index) {
 		this.index = index;
 	}
-	public int getUser_no() {
-		return user_no;
+
+	public String getUser_nick() {
+		return user_nick;
 	}
-	public void setUser_no(int user_no) {
-		this.user_no = user_no;
+	public void setUser_nick(String user_nick) {
+		this.user_nick = user_nick;
 	}
 	public String getContent() {
 		return content;
@@ -53,6 +54,16 @@ public class ChatVO {
 	public void setEmo(String emo) {
 		this.emo = emo;
 	}
+	@Override
+	public String toString() {
+		return "ChatVO [index=" + index + ", user_nick=" + user_nick + ", content=" + content + ", date=" + date
+				+ ", read_is=" + read_is + ", file=" + file + ", emo=" + emo + "]";
+	}
+	
+	
+
+	
+	
 	
 	
 	
